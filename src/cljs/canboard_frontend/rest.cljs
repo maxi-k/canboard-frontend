a3eabe8fa7047c2483fc89fb047956bb46ec3763
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-03-02 02:23:18 +0100

    Start refining rest interfacing

--- src/cljs/canboard_frontend/rest.cljs
+++ src/cljs/canboard_frontend/rest.cljs
@@ -1,16 +1,18 @@
 (ns canboard-frontend.rest
   (:require [ajax.core :refer [GET POST]]))
 
-(def server-url "localhost:3000")
-(def api-base-path (str server-url "/v1/"))
+(def api-base-path (str "/v1/"))
 
 (defn api-call [method path params]
-  (method (str api-base-path method) params))
+
+  (method (str api-base-path path) params))
 
 (defn sign-in-user [name passwd callback]
   "Signs in given user with name and password."
-  (api-call #'GET "auth/sign_in"
+  (api-call #'POST "auth/sign_in"
             {:handler callback
+             :crossDomain true
              :error-handler callback
+             :method :json
              :params {:email name
                       :password passwd}}))
